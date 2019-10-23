6b577c6a25b6d23d198822eeddfd250607457ef5
Álvaro Galisteo <alvarogalisteo@outlook.es>
2019-10-23 16:30:28 +0200

    Added installer

---
+++ install_gem5.sh
@@ -0,0 +1,14 @@
+# Update and install dependencies
+sudo apt-get update
+sudo apt-get install swig gcc m4 python python-dev libgoogle-perftools-dev g++ scons git zlib1g-dev protobuf-compiler libprotobuf-dev build-essential python-dev swig python-protobuf libgoogle-perftools-dev automake python-pip -y
+
+# Install python dependencies
+pip install six
+
+# Clone repository
+git clone https://gem5.googlesource.com/public/gem5
+
+# Get Full System files (For ALPHA and SPLASH Benchmarks). Currently unused
+wget http://www.m5sim.org/dist/current/m5_system_2.0b3.tar.bz2
+wget http://www.m5sim.org/dist/current/linux-dist.tgz
+wget http://www.gem5.org/dist/m5_benchmarks/v1-splash-alpha.tgz
