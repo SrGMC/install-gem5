c88d45a347d23a6b5f60a70f79d269e3f1733786
Álvaro Galisteo <alvarogalisteo@outlook.es>
2019-10-29 23:38:29 +0100

    Added ALPHA downloads

--- install_gem5.sh
+++ install_gem5.sh
@@ -8,10 +8,26 @@ pip install six
 # Clone repository
 git clone https://gem5.googlesource.com/public/gem5
 
-# Get Full System files (For ALPHA and SPLASH Benchmarks). Currently unused
+# Get PARSEC files
+cd gem5
+mkdir benchmarks
+cd benchmarks
+wget http://www.cs.utexas.edu/~parsec_m5/writescripts.pl
+wget http://www.cs.utexas.edu/~parsec_m5/inputsets.txt
+wget http://www.cs.utexas.edu/~parsec_m5/hack_back_ckpt.rcS
+# Download full system files
 wget http://www.m5sim.org/dist/current/m5_system_2.0b3.tar.bz2
-wget http://www.m5sim.org/dist/current/linux-dist.tgz
-wget http://www.gem5.org/dist/m5_benchmarks/v1-splash-alpha.tgz
+tar xzf m5_system_2.0b3.tar.bz2
+echo "Open gem5/configs/common/SysPaths.py. path should be similar to this:"
+echo "  path = [ ... , '$(pwd)/m5_system_2.0b3' ]"
+echo "Add the following directory to the path variable:"
+echo "  $(pwd)/m5_system_2.0b3"
+cd ..
 
 # Compile ALPHA
-scons ./build/ALPHA/gem5.opt
+scons ./build/ALPHA/gem5.opt -j 2
+
+echo "Done!"
+echo "Run simulations with the following command":
+echo "  ./build/ALPHA/gem5.opt configs/example/fs.py -n 1 --cpu-type=MinorCPU --caches --kernel=vmlinux"
+echo "Feel free to modify and execute diferent scripts"
