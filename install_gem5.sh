02f3d329b9e89884ae5c931bc4d7168526e2ec9c
Álvaro Galisteo <SrGMC@users.noreply.github.com>
2020-01-20 21:22:39 +0100

    Add support for ISA

--- install_gem5.sh
+++ install_gem5.sh
@@ -1,19 +1,37 @@
 #! /bin/bash
-if [[ $1 == "--help" ]]; then
+
+function help() {
 	echo "gem5 installer for Ubuntu with PARSEC benchmarks"
 	echo ""
-	echo "$0 [--help] [number of cores]"
+	echo "$0 [--help] <number of cores> <architecture>"
 	echo "  --help               Display this message"
-	echo "  <number of cores>    Specify how many cores to use for compilation. If not specified, 2 cores are used"
+	echo "  <number of cores>    Specify how many cores to use for compilation."
+	echo "  <architecture>       Specify the architecture ISA to compile. Currently available: "
+	echo "                       \{ALPHA, ARM, MIPS, POWER, SPARC, X86\}"
 	exit
+}
+
+function checkISA() {
+       if [[ $1 == "ALPHA" ]] || [[ $1 == "ARM" ]] || [[ $1 == "MIPS" ]] || [[ $1 == "POWER" ]] || [[ $1 == "SPARC" ]] || [[ $1 == "X86" ]]; then
+               echo "[INFO] Using $ISA for compilation"
+       else
+               echo "[ERROR] Unknown ISA $ISA"
+	       echo ""
+	       help
+	       exit
+}
+
+if [[ $1 == "--help" ]]; then
+	help
 fi
 
-if [[ $# -ne 1 ]]; then
-	echo "[INFO] Using 2 cores for compilation"
-	CORES=2
+if [[ $# -ne 2 ]]; then
+	help
 else
 	CORES=$1
+	ISA=$2
 	echo "[INFO] Using $CORES cores for compilation"
+	checkISA $ISA
 fi
 
 function downloadFiles() {
@@ -85,10 +103,10 @@ mv linux-parsec-2-1-m5-with-test-inputs.img m5_system_2.0b3/disks/linux-latest.i
 
 cd ..
 
-# Compile ALPHA
-echo "[INFO] Compiling gem5 with ALPHA ISA"
+# Compile ISA
+echo "[INFO] Compiling gem5 with $ISA ISA"
 echo "       This will take a long time. Get something to eat meanwhile"
-scons ./build/ALPHA/gem5.opt -j "$CORES"
+scons ./build/$ISA/gem5.opt -j "$CORES"
 
 if [[ $? -ne 0 ]]; then
 	echo "[ERROR] An error ocurred while compiling"
@@ -103,6 +121,6 @@ echo "  path = [ ... , '$(pwd)/benchmarks/m5_system_2.0b3' ]"
 echo "Add the following directory to the path variable:"
 echo "  $(pwd)/benchmarks/m5_system_2.0b3"
 echo " "
-echo "Run simulations with the following command":
-echo "  ./build/ALPHA/gem5.opt configs/example/fs.py -n 1 --cpu-type=MinorCPU --caches --kernel=vmlinux"
+echo "Run full system simulations with the following command":
+echo "  ./build/$ISA/gem5.opt configs/example/fs.py -n 1 --cpu-type=MinorCPU --caches --kernel=vmlinux"
 echo "Feel free to modify and execute diferent scripts"
